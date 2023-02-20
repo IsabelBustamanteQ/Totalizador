0da9c739d1b4a4a4a3f62d6f5ffd303a3f270150
Isabel <isabqm121@gmail.com>
2023-02-19 22:13:46 -0400

    calcular y mostrar precio total con impuesto

--- src/presenterTotalizador.js
+++ src/presenterTotalizador.js
@@ -7,38 +7,42 @@ form.addEventListener("submit", (event) => {
   var precio=document.querySelector("#precio").value;
   var estado=document.getElementById("estado");
   var codigoEstado=estado.options[estado.selectedIndex].text;
-  var impuesto;
+  var porcentajeImpuesto;
   var precioNeto=precio*cantidad;
   if(codigoEstado==="UT")
   {
-    impuesto=0.0665;
+    porcentajeImpuesto=0.0665;
   }
   else
   {
     if(codigoEstado==="NV")
     {
-        impuesto=0.08;
+        porcentajeImpuesto=0.08;
     }
     else
     {
         if(codigoEstado==="TX")
         {
-            impuesto=0.0625;
+            porcentajeImpuesto=0.0625;
         }
         else
         {
             if(codigoEstado==="AL")
             {
-                impuesto=0.04;
+                porcentajeImpuesto=0.04;
             }
             else
             {
-                impuesto=0.0825;
+                porcentajeImpuesto=0.0825;
             }
         }
     }
   }
-  var tPrecioNeto="Precio neto: $"+precioNeto;
-  div.innerHTML = tPrecioNeto;
+  impuesto=precioNeto*porcentajeImpuesto;
+  var precioTotal=precioNeto+impuesto;
+  var tPrecioNeto="<p>Precio neto: $"+precioNeto+"</p>";
+  var tImpuesto="<p>Impuesto para "+codigoEstado+": $"+impuesto+"</p>";
+  var tPrecioTotal="<p> Precio total: $"+precioTotal+"</p>";
+  div.innerHTML = tPrecioNeto + tImpuesto+tPrecioTotal;
 });
 
