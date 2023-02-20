7f59c2303651de9a9fd17dedb278ec0f3764e772
Isabel <isabqm121@gmail.com>
2023-02-19 21:38:04 -0400

    seleccionar y mostrar estado

--- src/presenterTotalizador.js
+++ src/presenterTotalizador.js
@@ -5,6 +5,9 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   var cantidad=document.querySelector("#cantidad").value;
   var precio=document.querySelector("#precio").value;
-  div.innerHTML = "Cantidad: "+cantidad +", Precio:"+ precio;
+  var estado=document.getElementById("estado");
+  var codigoEstado=estado.options[estado.selectedIndex].text;
+  div.innerHTML = "<p> Cantidad: "+cantidad +"</p><p> Precio:"+ precio+"</p><p> Estado:"+codigoEstado+"</p>";
+  
 });
 
