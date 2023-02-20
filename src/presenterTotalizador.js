540b34887624a0ddc0b54f7e0d7002b493c847ec
Isabel <isabqm121@gmail.com>
2023-02-19 21:51:10 -0400

    mostrar impuesto por estado

--- src/presenterTotalizador.js
+++ src/presenterTotalizador.js
@@ -7,7 +7,36 @@ form.addEventListener("submit", (event) => {
   var precio=document.querySelector("#precio").value;
   var estado=document.getElementById("estado");
   var codigoEstado=estado.options[estado.selectedIndex].text;
-  div.innerHTML = "<p> Cantidad: "+cantidad +"</p><p> Precio:"+ precio+"</p><p> Estado:"+codigoEstado+"</p>";
-  
+  var impuesto;
+  if(codigoEstado==="UT")
+  {
+    impuesto=0.0665;
+  }
+  else
+  {
+    if(codigoEstado==="NV")
+    {
+        impuesto=0.08;
+    }
+    else
+    {
+        if(codigoEstado==="TX")
+        {
+            impuesto=0.0625;
+        }
+        else
+        {
+            if(codigoEstado==="AL")
+            {
+                impuesto=0.04;
+            }
+            else
+            {
+                impuesto=0.0825;
+            }
+        }
+    }
+  }
+  div.innerHTML = "<p> Cantidad: "+cantidad +"</p><p> Precio:"+ precio+"</p><p> Estado:"+codigoEstado+"</p> <p> Impuesto: "+impuesto*100+"% </p>";
 });
 
